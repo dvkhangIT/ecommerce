cd8c77cf710c327f8baf36d8451c179f714efe9a
dvkhangIT <khangduong.dev@gmail.com>
2025-07-01 14:34:46 +0700

    44 - Ecommerce Multi Auth Setup With Breeze

---
+++ routes/vendor.php
@@ -0,0 +1,7 @@
+<?php
+// Vendor route
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
