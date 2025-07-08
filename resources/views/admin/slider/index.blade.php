5e09eab593f705be244a557792d65b3310b43f8b
dvkhangIT <khangduong.dev@gmail.com>
2025-07-08 19:16:51 +0700

    49 - Slider - Backend slider create page design

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -3,11 +3,6 @@
   <section class="section">
     <div class="section-header">
       <h1>Table</h1>
-      <div class="section-header-breadcrumb">
-        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-        <div class="breadcrumb-item"><a href="#">Components</a></div>
-        <div class="breadcrumb-item">Table</div>
-      </div>
     </div>
 
     <div class="section-body">
@@ -17,7 +12,8 @@
             <div class="card-header">
               <h4>Simple Table</h4>
               <div class="card-header-action">
-                <a href="" class="btn btn-primary">+ Create</a>
+                <a href="{{ route('admin.slider.create') }}"
+                  class="btn btn-primary">+ Create</a>
               </div>
             </div>
             <div class="card-body">
