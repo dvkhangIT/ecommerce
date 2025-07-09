d7c614f7469ae9f8c17d7ee9047a0366c71a0944
dvkhangIT <khangduong.dev@gmail.com>
2025-07-09 14:24:00 +0700

    49 - Slider - Make create form dynamic

--- database/migrations/2025_07_08_180218_create_sliders_table.php
+++ database/migrations/2025_07_08_180218_create_sliders_table.php
@@ -6,22 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('sliders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('sliders', function (Blueprint $table) {
+      $table->id();
+      $table->text('banner')->nullable();
+      $table->string('type')->nullable();
+      $table->string('title')->nullable();
+      $table->string('starting_price')->nullable();
+      $table->string('btn_url')->nullable();
+      $table->integer('serial')->nullable();
+      $table->integer('status')->nullable();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('sliders');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('sliders');
+  }
 };
