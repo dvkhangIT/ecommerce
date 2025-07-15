179336725ee4f7d0249e7e88f4fd99547637380b
dvkhangIT <khangduong.dev@gmail.com>
2025-07-15 18:03:32 +0700

    50 - Subcategory - working with update feature

--- resources/views/admin/sub-category/edit.blade.php
+++ resources/views/admin/sub-category/edit.blade.php
@@ -2,40 +2,44 @@
 @section('content')
   <section class="section">
     <div class="section-header">
-      <h1>Category</h1>
+      <h1>Sub Category</h1>
     </div>
     <div class="section-body">
       <div class="row">
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h4>Edit Category</h4>
+              <h4>Create Sub Category</h4>
             </div>
             <div class="card-body">
-              <form action="{{ route('admin.category.update', $category->id) }}"
+              <form
+                action="{{ route('admin.sub-category.update', $subCategory->id) }}"
                 method="POST">
                 @csrf
                 @method('PUT')
                 <div class="form-group">
-                  <label>Icon</label>
-                  <div>
-                    <button class="btn btn-primary"
-                      data-selected-class="btn-danger"
-                      data-unselected-class="btn-info" role="iconpicker"
-                      data-icon="{{ $category->icon }}" name="icon"></button>
-                  </div>
+                  <label for="inputState">Category</label>
+                  <select id="inputState" class="form-control" name="category">
+                    <option value="">Select</option>
+                    @foreach ($categories as $category)
+                      <option
+                        {{ $category->id == $subCategory->category_id ? 'selected' : '' }}
+                        value="{{ $category->id }}">{{ $category->name }}
+                      </option>
+                    @endforeach
+                  </select>
                 </div>
                 <div class="form-group">
                   <label>Name</label>
                   <input type="text" class="form-control" name="name"
-                    value="{{ $category->name }}">
+                    value="{{ $subCategory->name }}">
                 </div>
                 <div class="form-group">
                   <label for="inputState">Status</label>
                   <select id="inputState" class="form-control" name="status">
-                    <option {{ $category->status == 1 ? 'selected' : '' }}
+                    <option {{ $subCategory->status == 1 ? 'selected' : '' }}
                       value="1">Active</option>
-                    <option {{ $category->status == 0 ? 'selected' : '' }}
+                    <option {{ $subCategory->status == 0 ? 'selected' : '' }}
                       value="0">Inactive</option>
                   </select>
                 </div>
