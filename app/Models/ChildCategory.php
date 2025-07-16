e56a4aec17253f679312384a70de510c4aebad9d
dvkhangIT <khangduong.dev@gmail.com>
2025-07-16 15:19:41 +0700

    50 - Child Category - Show created data at datatables

--- app/Models/ChildCategory.php
+++ app/Models/ChildCategory.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChildCategory extends Model
 {
-    use HasFactory;
+  use HasFactory;
+  public function category()
+  {
+    return $this->belongsTo(Category::class);
+  }
+  public function subCategory()
+  {
+    return $this->belongsTo(SubCategory::class);
+  }
 }
