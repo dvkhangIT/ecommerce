73dd85eb00b45d8dda98d378719ecbaccffcb954
dvkhangIT <khangduong.dev@gmail.com>
2025-08-30 09:09:43 +0700

    53 - Product - Show created datas at datatable

--- app/DataTables/VendorProductDataTable.php
+++ app/DataTables/VendorProductDataTable.php
@@ -22,7 +22,55 @@ class VendorProductDataTable extends DataTable
   public function dataTable(QueryBuilder $query): EloquentDataTable
   {
     return (new EloquentDataTable($query))
-      ->addColumn('action', 'vendorproduct.action')
+      ->addColumn('action', function ($query) {
+        $editBtn = '<a href="' . route('admin.products.edit', $query->id) . '" class="btn btn-primary"><i class="far fa-edit"></i></a>';
+        $deleteBtn = '<a href="' . route('admin.products.destroy', $query->id) . '" class="btn btn-danger ml-1 delete-item"><i class="far fa-trash-alt"></i></a>';
+        $moreBtn = '<div class="dropdown dropleft d-inline">
+                      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-cog"></i>
+                      </button>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item has-icon" href="' . route('admin.products-image-gallery.index', ['product' => $query->id]) . '"><i class="far fa-heart"></i> Image Gallery</a>
+                        <a class="dropdown-item has-icon" href="' . route('admin.products-variant.index', ['product' => $query->id]) . '"><i class="far fa-file"></i> Variants</a>
+                      </div>
+                    </div>';
+        return $editBtn . $deleteBtn . $moreBtn;
+      })
+      ->addColumn('status', function ($query) {
+        if ($query->status == 1) {
+          $button = '<div class="form-check form-switch">
+          <input checked class="form-check-input change-status" type="checkbox" id="status__product_vendor" data-id="' . $query->id . '">
+        </div>';
+        } else {
+          $button = '<div class="form-check form-switch">
+          <input class="form-check-input change-status" type="checkbox" id="status__product_vendor" data-id="' . $query->id . '">
+        </div>';
+        }
+        return $button;
+      })
+      ->addColumn('thumb_image', function ($query) {
+        return $imag = "<img width='70px' src='" . asset($query->thumb_image) . "'>";
+      })
+      ->addColumn('type', function ($query) {
+        switch ($query->product_type) {
+          case 'new_arrival':
+            return '<i class="badge bg-success">New Arrival</i>';
+            break;
+          case 'featured_product':
+            return '<i class="badge bg-warning">Featured Product</i>';
+            break;
+          case 'top_product':
+            return '<i class="badge bg-info">Top Product</i>';
+            break;
+          case 'best_product':
+            return '<i class="badge bg-danger">Best Product</i>';
+            break;
+          default:
+            return '<i class="badge bg-dark">None</i>';
+            break;
+        }
+      })
+      ->rawColumns(['action', 'thumb_image', 'status', 'type'])
       ->setRowId('id');
   }
 
@@ -62,15 +110,17 @@ class VendorProductDataTable extends DataTable
   public function getColumns(): array
   {
     return [
+      Column::make('id')->type('sting'),
+      Column::make('thumb_image')->width(150),
+      Column::make('name'),
+      Column::make('price')->type('string'),
+      Column::make('type')->type(100),
+      Column::make('status'),
       Column::computed('action')
         ->exportable(false)
         ->printable(false)
-        ->width(60)
+        ->width(200)
         ->addClass('text-center'),
-      Column::make('id'),
-      Column::make('add your columns'),
-      Column::make('created_at'),
-      Column::make('updated_at'),
     ];
   }
 
