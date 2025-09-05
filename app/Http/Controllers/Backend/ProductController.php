d7720f51acd001ac2039b9cb33f749ebbd5d6142
dvkhangIT <khangduong.dev@gmail.com>
2025-09-05 08:38:12 +0700

    54 - Seller Product - Making changes on update feature

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -125,7 +125,6 @@ class ProductController extends Controller
     $imagePath = $this->updateImage($request, 'image', 'uploads', $product->thumb_image);
     $product->thumb_image = empty(!$imagePath) ? $imagePath : $product->thumb_image;
     $product->name = $request->name;
-    $product->vendor_id = Auth::user()->vendor->id;
     $product->category_id = $request->category;
     $product->sub_category_id = $request->sub_category;
     $product->child_category_id = $request->child_category;
@@ -141,7 +140,6 @@ class ProductController extends Controller
     $product->offer_end_date = $request->offer_end_date;
     $product->product_type = $request->product_type;
     $product->status = $request->status;
-    $product->is_approved = 1;
     $product->seo_title = $request->seo_title;
     $product->seo_description = $request->seo_description;
     $product->slug = Str::slug($request->name);
