7ea4c3d2cfd4297d792e974be9535a6ccb8098b4
dvkhangIT <khangduong.dev@gmail.com>
2025-10-05 20:48:31 +0700

    64 - 04 - 4_Payment gateway - Working with PayPal setting page (part - 2)

--- resources/views/admin/payment-settings/sections/paypal-setting.blade.php
+++ resources/views/admin/payment-settings/sections/paypal-setting.blade.php
@@ -1,52 +1,54 @@
 <div class="tab-pane fade active show" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
   <div class="card border">
     <div class="card-body">
-      <form action="{{ route('admin.general-setting-update') }}" method="POST">
+      <form action="{{ route('admin.paypal-setting.update', 1) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="form-group">
           <label>Paypal Status</label>
-          <select name="layout" class="form-control">
-            <option value="1">Enable</option>
-            <option value="0">Disable</option>
+          <select name="status" class="form-control">
+            <option {{ $paypalSetting->status === 1 ? 'selected' : '' }} value="1">Enable</option>
+            <option {{ $paypalSetting->status === 0 ? 'selected' : '' }} value="0">Disable</option>
           </select>
         </div>
         <div class="form-group">
           <label>Account Mode</label>
-          <select name="layout" class="form-control">
-            <option value="0">Sandbox</option>
-            <option value="1">Live</option>
+          <select name="mode" class="form-control">
+            <option {{ $paypalSetting->mode === 0 ? 'selected' : '' }} value="0">Sandbox</option>
+            <option {{ $paypalSetting->mode === 1 ? 'selected' : '' }} value="1">Live</option>
           </select>
         </div>
         <div class="form-group">
           <label>Contry Name</label>
-          <select name="layout" class="form-control select2">
+          <select name="country_name" class="form-control select2">
             <option value="">Select</option>
             @foreach (config('settings.country_list') as $country)
-              <option value="{{ $country }}">{{ $country }}</option>
+              <option {{ $paypalSetting->country_name === $country ? 'selected' : '' }} value="{{ $country }}">
+                {{ $country }}</option>
             @endforeach
           </select>
         </div>
         <div class="form-group">
           <label>Currency Name</label>
-          <select name="layout" class="form-control select2">
+          <select name="currency_name" class="form-control select2">
             <option value="">Select</option>
             @foreach (config('settings.currency_list') as $key => $currency)
-              <option value="{{ $currency }}">{{ $key }}</option>
+              <option {{ $paypalSetting->currency_name === $currency ? 'selected' : '' }} value="{{ $currency }}">
+                {{ $key }}</option>
             @endforeach
           </select>
         </div>
         <div class="form-group">
           <label>Currency rate (Per USD)</label>
-          <input type="text" value="" class="form-control" name="currency_rate">
+          <input type="text" value="{{ $paypalSetting->currency_rate }}" class="form-control" name="currency_rate">
         </div>
         <div class="form-group">
           <label>Paypal Client Id</label>
-          <input type="text" value="" class="form-control" name="client_id">
+          <input type="text" value="{{ $paypalSetting->client_id }}" class="form-control" name="client_id">
         </div>
         <div class="form-group">
           <label>Paypal Secret Key</label>
-          <input type="text" value="" class="form-control" name="secret_key">
+          <input type="text" value="{{ $paypalSetting->secret_key }}" class="form-control" name="secret_key">
         </div>
         <button type="submit" class="btn btn-primary">Update</button>
       </form>
