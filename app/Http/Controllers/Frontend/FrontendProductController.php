4f87f97a43fde6c06f892bfab591ec8a8156fb1f
dvkhangIT <khangduong.dev@gmail.com>
2025-10-27 09:20:42 +0700

    66 - 19 - Product page - working with product page brand filtering

--- app/Http/Controllers/Frontend/FrontendProductController.php
+++ app/Http/Controllers/Frontend/FrontendProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\ChildCategory;
 use App\Models\Product;
@@ -56,9 +57,24 @@ class FrontendProductController extends Controller
                     return $query->where('price', '>=', $from)->where('price', '<=', $to);
                 })
                 ->paginate(12);
+        } elseif ($request->has('brand')) {
+            $brand = Brand::where('slug', $request->brand)->firstOrFail();
+            $products = Product::where([
+                'status' => 1,
+                'brand_id' => $brand->id,
+                'is_approved' => 1,
+            ])
+                ->when($request->has('range'), function ($query) use ($request) {
+                    $price = explode(';', $request->range);
+                    $from = $price[0];
+                    $to = $price[1];
+                    return $query->where('price', '>=', $from)->where('price', '<=', $to);
+                })
+                ->paginate(12);;
         }
         $categories = Category::where(['status' => 1])->get();
-        return view('frontend.pages.product', compact('products', 'categories'));
+        $brands = Brand::where(['status' => 1])->get();
+        return view('frontend.pages.product', compact('products', 'categories', 'brands'));
     }
     public function showProduct(string $slug)
     {
