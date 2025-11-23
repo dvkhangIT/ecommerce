b616300ec1aa4f66ca7574b7f49d1148d38b8918
Khang Duong Van <khangduong.dev@gmail.com>
2025-11-23 14:34:19 +0700

    71 - 08 - Backend- Vendor condition page in backend

--- routes/admin.php
+++ routes/admin.php
@@ -35,6 +35,7 @@ use App\Http\Controllers\Backend\StripeSettingController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\SubscribersController;
 use App\Http\Controllers\Backend\TransactionController;
+use App\Http\Controllers\Backend\VendorConditionController;
 use App\Http\Controllers\Backend\VendorListController;
 use App\Http\Controllers\Backend\VendorRequestController;
 use Illuminate\Support\Facades\Route;
@@ -160,6 +161,8 @@ Route::get('customer', [CustomerListController::class, 'index'])->name('customer
 Route::put('customer/change-status', [CustomerListController::class, 'changeStatus'])->name('customer.change-status');
 Route::get('vendor-list', [VendorListController::class, 'index'])->name('vendor-list.index');
 Route::put('vendor-list/change-status', [CustomerListController::class, 'changeStatus'])->name('vendor-list.change-status');
+Route::get('vendor-condition', [VendorConditionController::class, 'index'])->name('vendor-condition.index');
+Route::put('vendor-condition/update', [VendorConditionController::class, 'update'])->name('vendor-condition.update');
 
 // Subscribers routes
 Route::get('subscribers', [SubscribersController::class, 'index'])->name('subscribers.index');
