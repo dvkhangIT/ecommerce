55d0decca32333ccf0f16e60eddf126a5804ea13
Khang Duong Van <khangduong.dev@gmail.com>
2026-02-24 11:28:19 +0700

    76 - 12 - Fixing Discount Calculation

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -160,14 +160,14 @@ class CartController extends Controller
             $coupon = Session::get('coupon');
             $subTotal = getCartTotal();
             if ($coupon['discount_type'] === 'amount') {
-                $total = $subTotal - $coupon['discount'];
+                $total = max(0, $subTotal - $coupon['discount']);
                 return response([
                     'status' => 'success',
                     'cart_total' => $total,
                     'discount' => $coupon['discount']
                 ]);
             } elseif ($coupon['discount_type'] === 'percent') {
-                $discount = $subTotal - ($subTotal * $coupon['discount'] / 100);
+                $discount = ($subTotal * $coupon['discount'] / 100);
                 $total = $subTotal - $discount;
                 return response([
                     'status' => 'success',
