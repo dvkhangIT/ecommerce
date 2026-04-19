755c31044724c2a541007af6093baaca1bd718b5
Khang Duong Van <khangduong.dev@gmail.com>
2026-04-19 11:25:44 +0700

    78 - 08 - Site Optimization - Optimizing Category Slider One Section

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -9,7 +9,7 @@
   @include('frontend.home.sections.brand-slider')
   @include('frontend.home.sections.single-banner')
   @include('frontend.home.sections.hot-deals')
-  {{-- @include('frontend.home.sections.category-product-slider-one') --}}
+  @include('frontend.home.sections.category-product-slider-one')
   {{-- @include('frontend.home.sections.category-product-slider-two') --}}
   {{-- @include('frontend.home.sections.large_banner') --}}
   {{-- @include('frontend.home.sections.weekly-best-item') --}}
