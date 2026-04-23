b9cc0175afd7b0e96e69f97b5edf42a8f9706752
Khang Duong Van <khangduong.dev@gmail.com>
2026-04-23 10:10:35 +0700

    78 - 13 - Site Optimization -  Optimizing Blog Page

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -13,18 +13,18 @@ class BlogController extends Controller
     public function  blog(Request $request)
     {
         if ($request->has('search')) {
-            $blogs = Blog::where('title', 'like', '%' . $request->search . '%')
+            $blogs = Blog::with('category')->where('title', 'like', '%' . $request->search . '%')
                 ->where('status', 1)
                 ->orderBy('id', 'DESC')
                 ->paginate(12);
         } elseif ($request->has('category')) {
             $category = BlogCategory::where('status', 1)->where('slug', $request->category)->firstOrFail();
-            $blogs = Blog::where('category_id', $category->id)
+            $blogs = Blog::with('category')->where('category_id', $category->id)
                 ->where('status', 1)
                 ->orderBy('id', 'DESC')
                 ->paginate(12);
         } else {
-            $blogs = Blog::where('status', 1)->orderBy('id', 'DESC')->paginate(12);
+            $blogs = Blog::with('category')->where('status', 1)->orderBy('id', 'DESC')->paginate(12);
         }
         return view('frontend.pages.blog', compact('blogs'));
     }
