0d6e7526bbce328723dae3bb01de9b54945d68d3
Khang Duong Van <khangduong.dev@gmail.com>
2026-06-11 19:07:56 +0700

    79 - 34 - Messaging System - Setting Dynamic Pusher Config (Part - 1)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\EmailConfiguration;
 use App\Models\GeneralSetting;
 use App\Models\LogoSetting;
+use App\Models\PusherSetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\View;
@@ -29,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
         // set time zone
         $generalSetting = GeneralSetting::first();
         $logoSetting = LogoSetting::first();
+        $pusherSetting = PusherSetting::first();
         Config::set('app.timezone', $generalSetting->time_zone);
 
         // set mail config
@@ -38,6 +40,12 @@ class AppServiceProvider extends ServiceProvider
         Config::set('mail.mailers.smtp.encryption', $mailSetting->encryption);
         Config::set('mail.mailers.smtp.port', $mailSetting->port);
         Config::set('mail.mailers.smtp.password', $mailSetting->password);
+
+        // set Broadcasting
+        Config::set('broadcasting.connections.pusher.key', $pusherSetting->pusher_key);
+        Config::set('broadcasting.connections.pusher.secret', $pusherSetting->pusher_secret);
+        Config::set('broadcasting.connections.pusher.app_id', $pusherSetting->pusher_app_id);
+        Config::set('broadcasting.connections.pusher.options.host', "api-" . "$pusherSetting->pusher_cluster" . ".pusher.com");
 
         // Share variable at all view
         View::composer('*', function ($view) use ($generalSetting, $logoSetting) {
